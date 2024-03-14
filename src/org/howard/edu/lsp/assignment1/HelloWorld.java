39aa99aa8fda1022171e582c5f53c547f4993781
aayushkananu <aayushkab.2003@gmail.com>
2024-03-14 17:54:49 -0400

    few changes

--- src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,10 +1,15 @@
 /**
-*  Name: Aatmann B C
+*  Name: Aayushka Budhathoki
 */
+
 package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
+
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
+		// TODO Auto-generated method stub
+
 	}
+
 }
